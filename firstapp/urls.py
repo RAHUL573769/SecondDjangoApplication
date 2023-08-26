ed54dbe337c831c68eeced92f95e99beab39b5d0
Rahul Rudra <rahulrudra665@gmail.com>
2023-08-26 11:16:15 +0600

    final commit

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from . import view
+from . import views
 
 urlpatterns = [
     # path("admin/", admin.site.urls),
-    path("/", (view.home)),
-    path("about/", (view.about)),
+    path("/", (views.home)),
+    path("about/", (views.about)),
 ]
